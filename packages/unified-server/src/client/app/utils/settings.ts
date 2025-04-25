181dfb1f1bea5f19484d9bb0f57a59b163c82e34
Gavin <jialehong@google.com>
2025-04-25 00:33:44 +0000

    Use indexDB as settings.

--- packages/unified-server/src/client/app/utils/settings.ts
+++ packages/unified-server/src/client/app/utils/settings.ts
@@ -3,39 +3,42 @@
  * Copyright 2025 Google LLC
  * SPDX-License-Identifier: Apache-2.0
  */
+import * as BreadboardUI from "@breadboard-ai/shared-ui";
+import { SettingsStore } from "@breadboard-ai/shared-ui/data/settings-store.js";
 
-import {
-  SettingEntry,
-  SETTINGS_TYPE,
-  SettingsHelper,
-} from "@breadboard-ai/shared-ui/types/types.js";
+// This one is the same as /usr/local/google/home/jialehong/Desktop/breadboard/packages/visual-editor/src/utils/settings-helper.ts.
+export class AgentspaceSettingsHelperImpl implements BreadboardUI.Types.SettingsHelper {
+#store: SettingsStore;
 
-export class SettingsHelperImpl implements SettingsHelper {
-  get(
-    _section: SETTINGS_TYPE,
-    name: string
-  ): SettingEntry["value"] | undefined {
-    try {
-      const item = globalThis.sessionStorage.getItem(name);
-      if (!item) {
-        return undefined;
-      }
-
-      return JSON.parse(item);
-    } catch (err) {
-      return undefined;
-    }
-  }
-
-  async set(
-    _section: SETTINGS_TYPE,
-    name: string,
-    value: SettingEntry["value"]
-  ): Promise<void> {
-    globalThis.sessionStorage.setItem(name, JSON.stringify(value));
-  }
-
-  async delete(_section: SETTINGS_TYPE, name: string) {
-    globalThis.sessionStorage.removeItem(name);
-  }
+constructor() {
+    this.#store = SettingsStore.instance();
 }
+
+get(
+    section: BreadboardUI.Types.SETTINGS_TYPE,
+    name: string
+): BreadboardUI.Types.SettingEntry["value"] | undefined {
+    return this.#store.values[section].items.get(name);
+}
+
+async set(
+    section: BreadboardUI.Types.SETTINGS_TYPE,
+    name: string,
+    value: BreadboardUI.Types.SettingEntry["value"]
+): Promise<void> {
+    console.log(`set ${name} as ${value}`);
+    const values = this.#store.values;
+    values[section].items.set(name, value);
+    await this.#store.save(values);
+}
+
+async delete(
+    section: BreadboardUI.Types.SETTINGS_TYPE,
+    name: string
+): Promise<void> {
+    const values = this.#store.values;
+    values[section].items.delete(name);
+    await this.#store.save(values);
+}
+}
+  
